f3a45aabd09daddc62785f5ad72b986044b2b494
goldbeef <goldbeef@163.com>
2017-03-11 08:33:34 +0800

    update tool

---
+++ simple_section.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int global_init_val = 84;
+int global_uninit_val;
+
+void func1(int i)
+{
+    printf("%d\n", i);
+}
+
+int main(void)
+{
+    static int static_init_val = 30;
+    static int static_uninit_val;
+
+    int a  = 1;
+    int b;
+    func1(static_init_val + static_uninit_val + a + b);
+    return a;
+}
