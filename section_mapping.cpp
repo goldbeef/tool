8b622b94a35c141da7da3ea757bc51f267dae2d0
goldbeef <goldbeef@163.com>
2017-04-08 10:21:09 +0800

    update

--- section_mapping.cpp
+++ section_mapping.cpp
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+ 
 int main(int argc, char* argv[])
 {
+    char stackBuff[1024*1024];
+    char *heapBuff = (char*) malloc(1024*1024);
+
     while(1)
     {
         sleep(1000);
