6cd375ccb5603c9ebdac5bd25fc29757ef8c8bab
goldbeef <goldbeef@163.com>
2017-05-07 11:58:24 +0800

    updte

---
+++ sleep.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: sleep.c
+    > Author: goldbeef
+    > Mail: goldbeef@163.com 
+    > Created Time: 2017年05月07日 星期日 11时52分02秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    while (1)
+    {
+        sleep(5);
+    }
+    return 0;
+}
